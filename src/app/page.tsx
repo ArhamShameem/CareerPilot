e6fe4fdc17e437915904463a2f8cf1b9a4a37a4c
Arham Shameem <golsushameem001@gmail.com>
2026-01-31 20:17:48 +0530

    Added Sidebar and completed clerk setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,34 @@
+import {
+  Sidebar,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebarClient } from "./_AppSidebarClient";
+
 export default function HomePage() {
-  return <>Hi</>
+  return (
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+      <Sidebar collapsible="icon" className="overflow-hidden">
+        <SidebarHeader className="flex-row">
+          <SidebarTrigger />
+          <span>CareerPilot</span>
+        </SidebarHeader>
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>dfghj</SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      </Sidebar>
+      <main className="flex-1">arham shameem</main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  );
 }
---
+++ src/hooks/useIsDarkMode.ts
@@ -0,0 +1,23 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
+export function useIsDarkMode() {
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    if (typeof window === "undefined") return false; // prevents crash during SSR
+    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+  });
+
+  useEffect(() => {
+    const controller = new AbortController();
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener(
+      "change",
+      (e) => {
+        setIsDarkMode(e.matches);
+      },
+      { signal: controller.signal }
+    );
+  });
+
+  return isDarkMode;
+}
